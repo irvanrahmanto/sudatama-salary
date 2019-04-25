f2728e98e15f66b15258d880c7539464fcbee82d
Irvan Naufali Rahmanto <42972909+irvanrahmanto@users.noreply.github.com>
2019-04-25 14:14:53 +0700

    header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -12,11 +12,11 @@
   <title><?= $title; ?></title>
 
   <!-- Custom fonts for this template-->
-  <link href="<?= base_url('assets/'); ?>fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
 
   <!-- Custom from load internet -->
   <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" rel="stylesheet">
